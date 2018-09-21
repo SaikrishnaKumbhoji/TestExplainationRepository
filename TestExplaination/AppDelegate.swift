e72365ae2b9f0eb7d94709016e973ba9c5e29da0
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-21 11:48:32 +0530

    Added a line in AppDelegate and VC

--- TestExplaination/AppDelegate.swift
+++ TestExplaination/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("New line has added by Saikrishna")
         return true
     }
 
