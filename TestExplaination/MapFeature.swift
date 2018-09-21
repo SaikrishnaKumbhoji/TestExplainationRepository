6213db1a75afae7d7602628d43ed37dc30602592
Sai Kumbhoji <saikrishnak@vs-macmini1.local>
2018-09-21 12:00:23 +0530

    Map Feature Added

---
+++ TestExplaination/MapFeature.swift
@@ -0,0 +1,15 @@
+//
+//  MapFeature.swift
+//  TestExplaination
+//
+//  Created by Sai Krishna Kumbhoji on 9/21/18.
+//  Copyright © 2018 Sai Krishna Kumbhoji. All rights reserved.
+//
+
+import UIKit
+
+class MapFeature: NSObject {
+    func hai(){
+        print("Hai")
+    }
+}
